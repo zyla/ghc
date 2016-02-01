b5df2cc6cf2af4508a4f34a718320a6d79f9adca
George Karachalias <george.karachalias@gmail.com>
2016-02-01 11:43:12 +0100

    Overhaul the Overhauled Pattern Match Checker
    * Changed the representation of Value Set Abstractions. Instead
      of using a prefix tree, we now use a list of Value Vector
      Abstractions. The set of constraints Delta for every Value
      Vector Abstraction is the oracle state so that we solve
      everything only once.
    
    * Instead of doing everything lazily, we prune at once (and in
      general everything is much stricter). A case writtern with
      pattern guards is not checked in almost the same time as the
      equivalent with pattern matching.
    
    * Do not store the covered and the divergent sets at all. Since
      what we only need is a yes/no (does this clause cover anything?
      Does it force any thunk?) We just keep a boolean for each.
    
    * Removed flags `-Wtoo-many-guards` and `-ffull-guard-reasoning`.
      Replaced with `fmax-pmcheck-iterations=n`. Still debatable what
      should the default `n` be.
    
    * When a guard is for sure not going to contribute anything, we
      treat it as such: The oracle is not called and cases `CGuard`,
      `UGuard` and `DGuard` from the paper are not happening at all
      (the generation of a fresh variable, the unfolding of the
      pattern list etc.). his combined with the above seems to be
      enough to drop the memory increase for test T783 down to 18.7%.
    
    * Do not export function `dsPmWarn` (it is now called directly
      from within `checkSingle` and `checkMatches`).
    
    * Make `PmExprVar` hold a `Name` instead of an `Id`. The term
      oracle does not handle type information so using `Id` was a
      waste of time/space.
    
    * Added testcases T11195, T11303b (data families) and T11374
    
    The patch addresses at least the following:
        #11195, #11276, #11303, #11374, #11162

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -531,9 +531,6 @@ data GeneralFlag
    -- safe haskell flags
    | Opt_DistrustAllPackages
    | Opt_PackageTrust
-
-   -- pm checking with guards
-   | Opt_FullGuardReasoning
    deriving (Eq, Show, Enum)
 
 data WarningFlag =
@@ -555,7 +552,6 @@ data WarningFlag =
    | Opt_WarnMissingLocalSigs
    | Opt_WarnNameShadowing
    | Opt_WarnOverlappingPatterns
-   | Opt_WarnTooManyGuards
    | Opt_WarnTypeDefaults
    | Opt_WarnMonomorphism
    | Opt_WarnUnusedTopBinds
@@ -642,6 +638,7 @@ data DynFlags = DynFlags {
   debugLevel            :: Int,         -- ^ How much debug information to produce
   simplPhases           :: Int,         -- ^ Number of simplifier phases
   maxSimplIterations    :: Int,         -- ^ Max simplifier iterations
+  maxPmCheckIterations  :: Int,         -- ^ Max no iterations for pm checking
   ruleCheck             :: Maybe String,
   strictnessBefore      :: [Int],       -- ^ Additional demand analysis
 
@@ -1435,6 +1432,7 @@ defaultDynFlags mySettings =
         debugLevel              = 0,
         simplPhases             = 2,
         maxSimplIterations      = 4,
+        maxPmCheckIterations    = 10000000,
         ruleCheck               = Nothing,
         maxRelevantBinds        = Just 6,
         simplTickFactor         = 100,
@@ -2608,6 +2606,8 @@ dynamic_flags = [
       (intSuffix (\n d -> d{ simplPhases = n }))
   , defFlag "fmax-simplifier-iterations"
       (intSuffix (\n d -> d{ maxSimplIterations = n }))
+  , defFlag "fmax-pmcheck-iterations"
+      (intSuffix (\n d -> d{ maxPmCheckIterations = n }))
   , defFlag "fsimpl-tick-factor"
       (intSuffix (\n d -> d{ simplTickFactor = n }))
   , defFlag "fspec-constr-threshold"
@@ -2956,7 +2956,6 @@ wWarningFlags = [
   flagSpec "orphans"                     Opt_WarnOrphans,
   flagSpec "overflowed-literals"         Opt_WarnOverflowedLiterals,
   flagSpec "overlapping-patterns"        Opt_WarnOverlappingPatterns,
-  flagSpec "too-many-guards"             Opt_WarnTooManyGuards,
   flagSpec "missed-specialisations"      Opt_WarnMissedSpecs,
   flagSpec "all-missed-specialisations"  Opt_WarnAllMissedSpecs,
   flagSpec' "safe"                       Opt_WarnSafe setWarnSafe,
@@ -3093,8 +3092,7 @@ fFlags = [
   flagSpec "unbox-strict-fields"              Opt_UnboxStrictFields,
   flagSpec "vectorisation-avoidance"          Opt_VectorisationAvoidance,
   flagSpec "vectorise"                        Opt_Vectorise,
-  flagSpec "worker-wrapper"                   Opt_WorkerWrapper,
-  flagSpec "full-guard-reasoning"             Opt_FullGuardReasoning
+  flagSpec "worker-wrapper"                   Opt_WorkerWrapper
   ]
 
 -- | These @-f\<blah\>@ flags can all be reversed with @-fno-\<blah\>@
