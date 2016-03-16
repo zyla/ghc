709b9233d086b4080bb859239cfba963c868877d
Ben Gamari <ben@smart-cactus.org>
2016-03-16 10:35:59 +0100

    Message: Import Data.Typeable.TypeRep

--- libraries/ghci/GHCi/Message.hs
+++ libraries/ghci/GHCi/Message.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, DeriveGeneric, StandaloneDeriving,
+{-# LANGUAGE GADTs, DeriveGeneric, StandaloneDeriving, CPP,
     GeneralizedNewtypeDeriving, ExistentialQuantification, RecordWildCards #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing -fno-warn-orphans #-}
 
@@ -37,6 +37,10 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as LB
 import Data.Dynamic
+#if MIN_VERSION_base(4,9,0)
+-- Previously this was re-exported by Data.Dynamic
+import Data.Typeable (TypeRep)
+#endif
 import Data.IORef
 import Data.Map (Map)
 import GHC.Generics
